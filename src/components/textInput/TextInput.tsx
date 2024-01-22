26d2a98c6600496b3578d15daae0e3ca6a90ce8a
svian <spearlvian@gmail.com>
2024-01-22 16:30:19 -0600

    on change

--- src/components/textInput/TextInput.tsx
+++ src/components/textInput/TextInput.tsx
@@ -4,6 +4,7 @@ interface TextInputProps {
   label: string;
   placeholder?: string;
   value?: string;
+  onChange?: (value: string | number | null | undefined) => void;
 }
 
 const TextInput: React.FC<TextInputProps> = (props: TextInputProps) => {
@@ -12,6 +13,9 @@ const TextInput: React.FC<TextInputProps> = (props: TextInputProps) => {
       label={props.label}
       placeholder={props.placeholder}
       value={props.value}
+      onChange={(event) =>
+        props.onChange && props.onChange(event.currentTarget.value)
+      }
     />
   );
 };
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Page from "../../components/page/Page";
 import TextInput from "../../components/textInput/TextInput";
 
 interface LoginProps {}
 
 const LoginPage: React.FC<LoginProps> = (props: LoginProps) => {
+  const [username, setUserName] = useState("");
   return (
     <Page>
-      <TextInput label="Username" placeholder="Username" />
+      <TextInput label="Username" placeholder="Username" value={username} />
       <TextInput label="Password" placeholder="Password" />
     </Page>
   );
