e6e5071fc2244845c5c3719bd1c3ca2b379972cc
svian <spearlvian@gmail.com>
2024-01-22 16:12:11 -0600

    Added value

--- src/components/textInput/TextInput.tsx
+++ src/components/textInput/TextInput.tsx
@@ -3,10 +3,17 @@ import { IonInput } from "@ionic/react";
 interface TextInputProps {
   label: string;
   placeholder?: string;
+  value?: string;
 }
 
 const TextInput: React.FC<TextInputProps> = (props: TextInputProps) => {
-  return <IonInput label={props.label} placeholder={props.placeholder} />;
+  return (
+    <IonInput
+      label={props.label}
+      placeholder={props.placeholder}
+      value={props.value}
+    />
+  );
 };
 
 export default TextInput;
