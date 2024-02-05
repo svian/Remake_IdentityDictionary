40dfc63d97c02fc39d96fd72125b80d6cb3c9048
svian <spearlvian@gmail.com>
2024-02-05 12:05:54 -0600

    Flags added

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -5,15 +5,32 @@ import {
   IonCardContent,
 } from "@ionic/react";
 import css from "./Card.module.css";
+import clsx from "clsx";
 
 interface CardProps {
   title: string;
+  color: "red" | "orange" | "yellow" | "green" | "white";
   children?: React.ReactNode;
 }
 
 const Card: React.FC<CardProps> = (props: CardProps) => {
+  function getColorCSS(): string {
+    switch (props.color) {
+      case "red":
+        return css.red;
+      case "orange":
+        return css.orange;
+      case "yellow":
+        return css.yellow;
+      case "green":
+        return css.green;
+      case "white":
+        return css.white;
+    }
+  }
+
   return (
-    <div className={css.card}>
+    <div className={clsx(css.card, getColorCSS())}>
       <IonCard>
         <IonCardHeader>
           <IonCardTitle className={css.title}>{props.title}</IonCardTitle>
